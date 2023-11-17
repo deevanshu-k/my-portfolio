32952f487b136bbea4371c00a82e20121a4f3109
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-11-17 23:53:44 +0530

    Sample template created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    // { path: "", redirectTo: "home", pathMatch: "prefix" },
+    { path: "", redirectTo: "home", pathMatch: "prefix" },
     { path: "home", loadComponent: () => import("./home/home.component").then(m => m.HomeComponent) },
     { path: "skills", loadComponent: () => import("./my-skills/my-skills.component").then(m => m.MySkillsComponent) },
     { path: "projects", loadComponent: () => import("./projects/projects.component").then(m => m.ProjectsComponent) },
